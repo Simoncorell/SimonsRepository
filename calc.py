e4eccf9aafafb25f5fc7091f38d5c0ec9a44e831
Simon Corell <simco664@student.liu.se>
2019-06-28 23:54:14 +0200

    Added calc.py

---
+++ calc.py
@@ -0,0 +1,14 @@
+def function(A, B, C):
+	print("A = ",A)
+	print("B = ",B)
+	print("C = ",C)
+	A = B
+	B = C
+	C = C
+	print("C = A = ", A)
+	return A*B*C
+
+value = function(1,2,3) 
+print("A*B*C=", value)
+
+
